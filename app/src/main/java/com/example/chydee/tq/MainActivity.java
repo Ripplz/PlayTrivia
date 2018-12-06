d9b4d3abe2ad78aa3184cb73659068e41e8cf325
Desmond <40004179+desmondmuqsit@users.noreply.github.com>
2018-12-06 02:19:48 +0100

    Initial commit. not what is expected

---
+++ app/src/main/java/com/example/chydee/tq/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.chydee.tq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Help(View view) {
+    }
+
+    public void Invite(View view) {
+    }
+
+    public void moreLives(View view) {
+    }
+
+    public void more(View view) {
+    }
+}
