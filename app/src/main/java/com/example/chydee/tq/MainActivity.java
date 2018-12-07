9383034c7cd478683e27269f81be1d4c5240d9f7
Stephen Ojo <ojostephendev@gmail.com>
2018-12-07 13:16:30 +0100

    I've run the race

--- app/src/main/java/com/example/chydee/tq/MainActivity.java
+++ app/src/main/java/com/example/chydee/tq/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.chydee.tq;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout llBottomSheet = findViewById(R.id.ll_bottomsheet);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        final LinearLayout top = findViewById(R.id.top);
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+                if (v > 0.5f)
+                    top.setAlpha(v);
+            }
+        });
     }
 
     public void Help(View view) {
